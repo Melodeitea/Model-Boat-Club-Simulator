2bb5202fa4470c223bbe25bcc416eb18952b2e2b
Melodeitea <118695764+Melodeitea@users.noreply.github.com>
2026-01-24 21:35:18 +0100

    Revert "mechanics"
    This reverts commit 8d065416fd9c2d9a882461301169c0a5799f0204.

--- Assets/Scripts/BoatBehaviourReference.cs
+++ Assets/Scripts/BoatBehaviourReference.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 
-/// <summary>
-/// Tiny component to attach to boat prefabs.
-/// Exposes a reference to the `BoatBehaviourConfig` ScriptableObject.
-/// </summary>
-[DisallowMultipleComponent]
 public class BoatBehaviourReference : MonoBehaviour
 {
-    [Header("Behaviour (ScriptableObject)")]
-    [Tooltip("Reference to the ScriptableObject that contains shared behaviour parameters for this boat/family.")]
-    [SerializeField]
-    private BoatBehaviourConfig behaviourConfig = null;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
-    // Public read-only accessor for runtime scripts (BoatAutoPilot will use this).
-    public BoatBehaviourConfig BehaviourConfig => behaviourConfig;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
